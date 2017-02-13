c64afd115e3501ac6442f27129a22ad033280d26
Estevan Diaz <sdiaz@login3.stampede.tacc.utexas.edu>
2017-02-12 22:35:23 -0600

    No papi timing and clock cycles added

--- no_papi_multiply.c
+++ no_papi_multiply.c
@@ -1,32 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <papi.h>
+#include <time.h>
 
-void print_counters(long long* counters) {
-    printf("%lld L1 Cache misses \n%lld L1 Cache Misses \n%lld L2 Cache Accesses \n%lld L2 Cache Misses \n%lld Load/Store Instructions \n%lld FP instructions \n%lld Cycles\n\n ",
-            counters[0],
-            counters[1],
-            counters[2],
-            counters[3],
-            counters[4],
-            counters[5],
-            counters[6] );
-}
+#define BILLION 1000000000L
 
-void matrix_matrix_multiply(int N, int* PAPI_events) {
+
+void matrix_matrix_multiply(int N) {
     double A[N][N], B[N][N], X[N][N];
     memset(A, 0, N*N*sizeof(double));
     memset(B, 0, N*N*sizeof(double));
     memset(X, 0, N*N*sizeof(double));
 
-    PAPI_library_init(PAPI_VER_CURRENT);
     int i, j, k;
-    int sum = 0;
-    long long counters[7];
+    struct timespec start, end;
+    long long diff = 0;
     printf("++++++++++++++\n");
     printf("+   i-j-k    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start);
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             for (k = 0; k < N; k++) {
@@ -34,17 +25,18 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters, 7);
-    print_counters(counters);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
 
 
     j, i, k = 0;
-    sum = 0;
-    long long counters_jik[7];
+    struct timespec start_jik, end_jik;
+    diff = 0;
     printf("++++++++++++++\n");
     printf("+   j-i-k    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start_jik);
     for (j = 0; j < N; j++) {
         for (i = 0; i < N; i++) {
             for (k = 0; k < N; k++) {
@@ -52,16 +44,18 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters_jik, 7);
-    print_counters(counters_jik);
+    clock_gettime(CLOCK_MONOTONIC, &end_jik);
+    diff = BILLION * (end_jik.tv_sec - start_jik.tv_sec) + end_jik.tv_nsec - start_jik.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
+
 
     j, k, i = 0;
-    sum = 0;
-    long long counters_jki[7];
+    struct timespec start_jki, end_jki;
+    diff = 0;
     printf("++++++++++++++\n");
     printf("+   j-k-i    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start_jki);
     for (j = 0; j < N; j++) {
         for (k = 0; k < N; k++) {
             for (i = 0; i < N; i++) {
@@ -69,16 +63,17 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters_jki, 7);
-    print_counters(counters_jki);
+    clock_gettime(CLOCK_MONOTONIC, &end_jki);
+    diff = BILLION * (end_jki.tv_sec - start_jki.tv_sec) + end_jki.tv_nsec - start_jki.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
 
     k, j, i = 0;
-    sum = 0;
-    long long counters_kji[7];
+    struct timespec start_kji, end_kji;
+    diff = 0;
     printf("++++++++++++++\n");
     printf("+   k-j-i    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start_kji);
     for (k = 0; k < N; k++) {
         for (j = 0; j < N; j++) {
             for (i = 0; i < N; i++) {
@@ -86,16 +81,17 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters_kji, 7);
-    print_counters(counters_kji);
+    clock_gettime(CLOCK_MONOTONIC, &end_kji);
+    diff = BILLION * (end_kji.tv_sec - start_kji.tv_sec) + end_kji.tv_nsec - start_kji.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
 
     i, k, j = 0;
-    sum = 0;
-    long long counters_ikj[7];
+    struct timespec start_ikj, end_ikj;
+    diff = 0;
     printf("++++++++++++++\n");
     printf("+   i-k-j    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start_ikj);
     for (i = 0; i < N; i++) {
         for (k = 0; k < N; k++) {
             for (j = 0; j < N; j++) {
@@ -103,16 +99,18 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters_ikj, 7);
-    print_counters(counters_ikj);
+    clock_gettime(CLOCK_MONOTONIC, &end_ikj);
+    diff = BILLION * (end_ikj.tv_sec - start_ikj.tv_sec) + end_ikj.tv_nsec - start_ikj.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
+
 
     k, i, j = 0;
-    sum = 0;
-    long long counters_kij[7];
+    struct timespec start_kij, end_kij;
+    diff = 0;
     printf("++++++++++++++\n");
     printf("+   k-i-j    +\n");
     printf("+  %d x %d     +\n", N, N);
-    PAPI_start_counters(PAPI_events, 7);
+    clock_gettime(CLOCK_MONOTONIC, &start_kij);
     for (k = 0; k < N; k++) {
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
@@ -120,31 +118,21 @@ void matrix_matrix_multiply(int N, int* PAPI_events) {
             }
         }
     }
-    PAPI_stop_counters(counters_kij, 7);
-    print_counters(counters_kij);
+    clock_gettime(CLOCK_MONOTONIC, &end_kij);
+    diff = BILLION * (end_kij.tv_sec - start_kij.tv_sec) + end_kij.tv_nsec - start_kij.tv_nsec;
+    printf("elapsed time = %llu nanoseconds\n\n", (long long unsigned int) diff);
 
 }
 
 int main(int args, char *argv[]) {
-    long long counters[7];
-    int PAPI_events[] = {
-        PAPI_L1_ICM,
-        PAPI_L1_DCM,
-        PAPI_L2_DCA,
-        PAPI_L2_DCM,
-        PAPI_LST_INS,
-        PAPI_FP_INS,
-        PAPI_TOT_CYC };
     int MATRIX_SIZES[] = {
         50, 
         200,
         2000 };
-    int num_counters = PAPI_num_counters();
 
-    printf("This system has %d available PAPI counters\n", num_counters);
     int i = 0;
     for (i = 0; i < 3; i++) {
-        matrix_matrix_multiply(MATRIX_SIZES[i], PAPI_events);
+        matrix_matrix_multiply(MATRIX_SIZES[i]);
     }
 
     return 0;
